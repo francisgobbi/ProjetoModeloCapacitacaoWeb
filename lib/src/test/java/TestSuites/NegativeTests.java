51117a95e1535b554e550eb25da236fbfbf92b97
francisgobbi <francis.gobbi@db.tec.br>
2023-11-17 09:13:54 -0300

    update suite tests negative positive tests

---
+++ lib/src/test/java/TestSuites/NegativeTests.java
@@ -0,0 +1,13 @@
+package TestSuites;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+import TestCases.RealizarLoginComUserInvalidoTest;
+
+@Suite
+@SelectClasses({RealizarLoginComUserInvalidoTest.class})
+public class NegativeTests {
+
+	
+}
---
+++ lib/src/test/java/TestSuites/PositiveTests.java
@@ -0,0 +1,13 @@
+package TestSuites;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import TestCases.RealizaCadastroUsuarioExistenteTest;
+
+
+@Suite
+@SelectClasses({RealizaCadastroUsuarioExistenteTest.class})
+public class PositiveTests {
+
+	
+}
